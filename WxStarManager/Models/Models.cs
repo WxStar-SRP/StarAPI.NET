fcf12c407484989c12dae8002ce231751daa45e1
mewtek <april.p@mistwx.com>
2025-06-10 23:36:21 -0700

    Fix incorrect types in API models

--- WxStarManager/Models/Models.cs
+++ WxStarManager/Models/Models.cs
@@ -1,34 +1,35 @@
 using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace WxStarManager.Models;
 
 public class WxStarIn
 {
-    [JsonPropertyName("name")] public string? Name { get; set; }
-    [JsonPropertyName("model")] public WxStarModel Model { get; set; } = WxStarModel.NoneSpecified;
-    [JsonPropertyName("ip_addr")] public string IpAddress { get; set; } = "127.0.0.1";
-    [JsonPropertyName("data_port")] public int DataPort { get; set; } = 7777;
-    [JsonPropertyName("data_port_pri")] public int DataPortPri { get; set; } = 7788;
-    [JsonPropertyName("gfxpkg_lf")] public string? GfxPkgLf { get; set; } = null;
-    [JsonPropertyName("gfxpkg_ldl")] public string? GfxPkgLdl { get; set; } = null;
+    [JsonProperty("name")] public string? Name { get; set; }
+    [JsonProperty("model")] public string Model { get; set; } = "IntelliStar2";
+    [JsonProperty("ip_addr")] public string IpAddress { get; set; } = "127.0.0.1";
+    [JsonProperty("data_port")] public string DataPort { get; set; } = "7777";
+    [JsonProperty("data_port_pri")] public string DataPortPri { get; set; } = "7787";
+    [JsonProperty("gfxpkg_lf")] public string? GfxPkgLf { get; set; } = null;
+    [JsonProperty("gfxpkg_ldl")] public string? GfxPkgLdl { get; set; } = null;
 }
 
 public class SystemServiceIn
 {
-    [JsonPropertyName("name")] public string? Name { get; set; }
-    [JsonPropertyName("host")] public string Host { get; set; } = "127.0.0.1";
-    [JsonPropertyName("pid")] public int? ProcessId { get; set; }
+    [JsonProperty("name")] public string? Name { get; set; }
+    [JsonProperty("host")] public string Host { get; set; } = "127.0.0.1";
+    [JsonProperty("pid")] public int? ProcessId { get; set; }
 }
 
 public class SystemServiceReport
 {
-    [JsonPropertyName("online")] public bool Online { get; set; } = false;
-    [JsonPropertyName("update_timestamp")] public DateTime UpdateTimestamp { get; set; } = DateTime.Now;
-    [JsonPropertyName("json_stats")] public string? JsonStats { get; set; } = null;
+    [JsonProperty("online")] public bool Online { get; set; } = false;
+    [JsonProperty("update_timestamp")] public DateTime UpdateTimestamp { get; set; } = DateTime.Now;
+    [JsonProperty("json_stats")] public string? JsonStats { get; set; } = null;
 }
 
 public class WxStarLocationUpdate
 {
-    [JsonPropertyName("locations")] public List<string> Locations { get; set; } = new();
-    [JsonPropertyName("zones")] public List<string>? Zones { get; set; }
+    [JsonProperty("locations")] public List<string> Locations { get; set; } = new();
+    [JsonProperty("zones")] public List<string>? Zones { get; set; }
 }
