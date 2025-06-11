91947f3b17934b54d72388e0959367d99d8e369b
mewtek <april.p@mistwx.com>
2025-06-10 23:17:05 -0700

    Use Newtonsoft to convert objects to HTTPContent objects before sending post/put requests to the API

--- WxStarManager/Api.Wxstar.cs
+++ WxStarManager/Api.Wxstar.cs
@@ -1,4 +1,7 @@
 using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 using WxStarManager.Models;
 
 namespace WxStarManager;
@@ -7,7 +10,9 @@ public partial class Api
 {
     public async Task<StarInfo> RegisterStar(WxStarIn inModel)
     {
-        var response = await _client.PostAsJsonAsync($"{Uri}/wxstar/register", inModel); 
+        var serializedModel = JsonConvert.SerializeObject(inModel);
+        var content = new StringContent(serializedModel, Encoding.UTF8, "application/json");
+        var response = await _client.PostAsync($"{Uri}/wxstar/register", content);
         response.EnsureSuccessStatusCode();
         var responseContent = await response.Content.ReadFromJsonAsync<StarInfo>();
 
@@ -53,8 +58,9 @@ public partial class Api
     public async Task UpdateUnitLocations(string starUuid, List<string> locations, List<string>? zones = null)
     {
         var locationUpdate = new WxStarLocationUpdate() { Locations = locations, Zones = zones };
+        var content = new StringContent(JsonConvert.SerializeObject(locationUpdate), Encoding.UTF8, "application/json");
 
-        var response = await _client.PutAsJsonAsync($"{Uri}/wxstar/{starUuid}/set_locations", locationUpdate);
+        var response = await _client.PutAsync($"{Uri}/wxstar/{starUuid}/set_locations", content);
         response.EnsureSuccessStatusCode();
     }
 }
